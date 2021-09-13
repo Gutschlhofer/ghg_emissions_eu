03867338c7698ddf3174dbddeb1519bfbfbadf35
Jakob Gutschlhofer <jakob.gutschlhofer@wu.ac.at>
2021-09-13 14:53:01 +0000

    09: add output for model results overview

--- code/09_model_spatial.R
+++ code/09_model_spatial.R
@@ -1,3 +1,79 @@
+base_variables <- c(
+  "log(pop)",
+  # "log(pop_share_Y15_64)",
+  # "log(pop_share_Y20_34)",
+  # "log(pop_share_Y35_49)",
+  # "log(pop_share_Y50_64)",
+  "log(pop_share_Y_GE65)",
+  "log(density)",
+  "log_gdppc",
+  # "I(log_gdppc^2)",
+  "log(gva_share_A)",
+  "log(gva_share_BE)",
+  "log(gva_share_F)",
+  # "log(gva_share_GJ)",
+  "log(hdd)",
+  "log(cdd_fix)",
+  "log(REN)",
+  "urbn_type",
+  "coast_type" #,
+  # "mount_type"
+)
+
+dep_var <- "edgar"
+dep_variable <- sprintf("log(%s)", dep_var)
+
+model_base <- as.formula(paste(dep_variable, "~", paste(base_variables, collapse= "+")))
+
+lw_spatial <- lw_inversedist
+
+model_ols <- lm(model_base, data)
+
+model_sdm <- lagsarlm(model_base, data = data, listw = lw_spatial, Durbin = TRUE)
+
+model_sem <- spatialreg::errorsarlm(model_base, data = data, listw = lw_spatial)
+
+model_sac <- sacsarlm(model_base, data = data, listw = lw_spatial)
+
+model_sar <- lagsarlm(model_base, data = data, listw = lw_spatial, Durbin = FALSE)
+
+model_sdem <- spatialreg::errorsarlm(model_base, data = data, listw = lw_spatial, etype = "emixed")
+
+model_gns <- spatialreg::sacsarlm(model_base, data = data, listw = lw_spatial, type="sacmixed")
+
+summary(model_sem)
+summary(model_sar)
+stargazer(model_sar, type = "text", single.row = T, digits = 2)
+
+summary(model_sac)
+
+summary(model_sdm)
+stargazer(model_sdm, type = "text", single.row = T, digits = 2)
+summary(model_sdem)
+# summary(model_sdm)
+
+spatialreg::LR.Sarlm(model_sdm, model_sem)
+
+round_impacts <- function(model, listw, digits = 2) {
+  df <- impacts(model, listw = listw)
+  is_sdem <- FALSE
+  if(!is.null(df$impacts)) {
+    df <- df$impacts
+    is_sdem <- TRUE
+  }
+  df$direct <- round(df$direct, digits = digits)
+  df$indirect <- round(df$indirect, digits = digits)
+  df$total <- round(df$total, digits = digits)
+  if(is_sdem) df <- df %>% as.data.frame()
+  return(df)
+}
+t_impacts <- Matrix(nrow = length(base_variables)+2, ncol = 4, data = 0)
+t_impacts[,1] <- round_impacts(model = model_sar, listw = lw_spatial, digits = 2)$total
+t_impacts[,2] <- round_impacts(model = model_sac, listw = lw_spatial, digits = 2)$total
+t_impacts[,3] <- round_impacts(model = model_sdm, listw = lw_spatial, digits = 2)$total
+t_impacts[,4] <- round_impacts(model = model_sdem, listw = lw_spatial, digits = 2)$total
+
+stargazer(t_impacts %>% as.matrix(), digits = 2)
 
 lw_spatial <- lw_queen
 lw_spatial <- lw_inversedist
@@ -7,7 +83,7 @@ dep_variable <- "log(edgar)"
 # 1. Model with everything
 base_variables <- c(
   "log(pop)",
-  "log(pop_share_Y15_64)",
+  # "log(pop_share_Y15_64)",
   "log(pop_share_Y_GE65)",
   "log(density)",
   "log_gdppc",
@@ -15,7 +91,7 @@ base_variables <- c(
   "log(gva_share_A)",
   "log(gva_share_BE)",
   "log(gva_share_F)",
-  "log(gva_share_GJ)",
+  # "log(gva_share_GJ)",
   "log(hdd)",
   "log(cdd_fix)",
   "log(REN)",
@@ -105,25 +181,6 @@ data_ev <- data %>% cbind(ev_sel)
 dep_var <- "edgar"
 
 dep_variable <- sprintf("log(%s)", dep_var) #"log(edgar)"
-
-base_variables <- c(
-  "log(pop)",
-  "log(pop_share_Y15_64)",
-  "log(pop_share_Y_GE65)",
-  "log(density)",
-  "log_gdppc",
-  "I(log_gdppc^2)",
-  "log(gva_share_A)",
-  "log(gva_share_BE)",
-  "log(gva_share_F)",
-  "log(gva_share_GJ)",
-  "log(hdd)",
-  "log(cdd_fix)",
-  "log(REN)",
-  "urbn_type",
-  "coast_type" #,
-  # "mount_type"
-)
 
 model_base_ev <- as.formula(paste(dep_variable, "~", paste(c(base_variables, paste0("ev", 1:n_ev)), collapse= "+")))
 
